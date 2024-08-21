6035d569463bc7f64fb75ae1cbad4da47513e84e
novoLife <mahajana@foxmail.com>
2024-08-21 10:44:20 +0800

    Update nrf_calendar.h

--- nrf_calendar.h
+++ nrf_calendar.h
@@ -1,15 +1,3 @@
-/* Copyright (c) 2014 Nordic Semiconductor. All Rights Reserved.
- *
- * The information contained herein is property of Nordic Semiconductor ASA.
- * Terms and conditions of usage are described in detail in NORDIC
- * SEMICONDUCTOR STANDARD SOFTWARE LICENSE AGREEMENT.
- *
- * Licensees are granted free, non-transferable use of the information. NO
- * WARRANTY of ANY KIND is provided. This heading must NOT be removed from
- * the file.
- *
- */
-
 #ifndef __NRF_CALENDAR_H__
 #define __NRF_CALENDAR_H__
 
